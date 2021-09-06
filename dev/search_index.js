4346e548f90853b66475af1c3b2e17ff6d864260
Documenter.jl <documenter@juliadocs.github.io>
2021-09-06 22:32:47 +0000

    build based on a7aa91e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = nMDS","category":"page"},{"location":"#nMDS","page":"Home","title":"nMDS","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for nMDS.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [nMDS]","category":"page"}]
+}
